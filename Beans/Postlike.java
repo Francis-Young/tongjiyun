8c9c3e6d0f7ba1996de182d56f0f4b2ff32ab306
cliture <52910884+cliture@users.noreply.github.com>
2019-07-16 21:21:59 +0800

    Add files via upload

---
+++ Beans/Postlike.java
@@ -0,0 +1,26 @@
+package beans;
+
+public class Postlike {
+	private int polikeid;
+	private int poid;
+	private int sid;
+	public int getPolikeid() {
+		return polikeid;
+	}
+	public void setPolikeid(int polikeid) {
+		this.polikeid = polikeid;
+	}
+	public int getPoid() {
+		return poid;
+	}
+	public void setPoid(int poid) {
+		this.poid = poid;
+	}
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	
+}
