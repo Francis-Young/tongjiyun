6360bd4ee3705332632c3079f02dd5e3e435bd96
Francis-Young <47942650+Francis-Young@users.noreply.github.com>
2019-07-16 20:41:52 +0800

    Add files via upload

---
+++ Beans/Score.java
@@ -0,0 +1,36 @@
+package beans;
+
+public class Score {
+	private int scoreid;
+	private int csid;
+	private int resid;
+	private int score;
+	public int getScoreid() {
+		return scoreid;
+	}
+	public void setScoreid(int scoreid) {
+		this.scoreid = scoreid;
+	}
+	public int getCsid() {
+		return csid;
+	}
+	public void setCsid(int csid) {
+		this.csid = csid;
+	}
+	public int getResid() {
+		return resid;
+	}
+	public void setResid(int resid) {
+		this.resid = resid;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+
+
+	
+}
