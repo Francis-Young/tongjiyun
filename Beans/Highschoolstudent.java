ca47407fca96fb3afd2c29c6aac4b4769e0de55d
cliture <52910884+cliture@users.noreply.github.com>
2019-07-16 21:38:08 +0800

    Add files via upload

---
+++ Beans/Highschoolstudent.java
@@ -0,0 +1,61 @@
+package beans;
+
+public class Highschoolstudent {
+	private int hsid;
+	private String hspassword=new String();
+	private String hsname=new String();
+	private String hsphone=new String();
+	private int hssex;
+	private String hsregion=new String();
+	private int hsgaokao_year;
+	private String hschoice=new String();
+	public int getHsid() {
+		return hsid;
+	}
+	public void setHsid(int hsid) {
+		this.hsid = hsid;
+	}
+	public String getHspassword() {
+		return hspassword;
+	}
+	public void setHspassword(String hspassword) {
+		this.hspassword = hspassword;
+	}
+	public String getHsname() {
+		return hsname;
+	}
+	public void setHsname(String hsname) {
+		this.hsname = hsname;
+	}
+	public String getHsphone() {
+		return hsphone;
+	}
+	public void setHsphone(String hsphone) {
+		this.hsphone = hsphone;
+	}
+	public int getHssex() {
+		return hssex;
+	}
+	public void setHssex(int hssex) {
+		this.hssex = hssex;
+	}
+	public String getHsregion() {
+		return hsregion;
+	}
+	public void setHsregion(String hsregion) {
+		this.hsregion = hsregion;
+	}
+	public int getHsgaokao_year() {
+		return hsgaokao_year;
+	}
+	public void setHsgaokao_year(int hsgaokao_year) {
+		this.hsgaokao_year = hsgaokao_year;
+	}
+	public String getHschoice() {
+		return hschoice;
+	}
+	public void setHschoice(String hschoice) {
+		this.hschoice = hschoice;
+	}
+	
+}
