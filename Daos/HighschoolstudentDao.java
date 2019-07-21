a55cbb974e4c0ab99bb3b8f925ab3a84b4803787
Francis-Young <47942650+Francis-Young@users.noreply.github.com>
2019-07-21 14:10:22 +0800

    Update HighschoolstudentDao.java

--- Daos/HighschoolstudentDao.java
+++ Daos/HighschoolstudentDao.java
@@ -65,4 +65,29 @@ public class HighschoolstudentDao {
 		//返回Highschoolstudent对象
 		return hs;
 	}
+	public boolean checkHighschoolstudentByHsname(String hsname) {
+
+		//建立数据库连接
+		Connection conn=DataBaseUtil.getConnection();
+
+		try {
+			//查询语句，根据账号查询
+			String sql=""+"select * from highschoolstudent where hsname = ?";
+			PreparedStatement psmt = conn.prepareStatement(sql);
+			psmt.setString(1, hsname);
+			//执行查询语句
+			ResultSet rs = psmt.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+		}catch(SQLException e) {
+            e.printStackTrace();
+        }catch(NullPointerException f){
+            f.printStackTrace();
+        }finally {
+            DataBaseUtil.closeConnection(conn);
+        }
+		//返回t or f
+		return false;
+	}
 }
