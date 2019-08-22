765e4a74b9027edb086d9d915f9bedbdcb758422
Francis-Young <47942650+Francis-Young@users.noreply.github.com>
2019-08-22 13:10:17 +0800

    Add files via upload

---
+++ 爱在同济/rank.java
@@ -0,0 +1,42 @@
+package beans;
+
+public class rank {
+	
+	private int rid;
+	private int frequency;
+	private int type;
+	private int islastest;
+	private String name;
+	public int getRid() {
+		return rid;
+	}
+	public void setRid(int rid) {
+		this.rid = rid;
+	}
+	public int getFrequency() {
+		return frequency;
+	}
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public int getIslastest() {
+		return islastest;
+	}
+	public void setIslastest(int islastest) {
+		this.islastest = islastest;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
