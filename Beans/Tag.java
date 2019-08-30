1166ef2aa959593b5da33a58aaa2c040335b01ba
Cloud-in-the-sky <52921725+Cloud-in-the-sky@users.noreply.github.com>
2019-08-30 08:58:34 +0800

    Add files via upload
    测试时发现原文件存在错误，更正后上传。

--- Beans/Tag.java
+++ Beans/Tag.java
@@ -1,46 +1,93 @@
 package beans;
 
+
+
 import java.sql.Time;
+import java.sql.Date;
+
+
 
 /**
+
  * @author lh
+
  *
+
  */
+
 public class Tag {
+
 	private int tagid;
+
 	private int csid;
+
 	private String text=new String();
+
 	private Time time=null;
+
 		private Date date=null;
-	public int getTagid() {
-		return tagid;
-	}
+
 	public Date getDate() {
+
 		return date;
-	}
-	public int getTagid() {
-		return tagid;
-	}
-	public void setTagid(int tagid) {
-		this.tagid = tagid;
-	}
-	public int getCsid() {
-		return csid;
-	}
-	public void setCsid(int csid) {
-		this.csid = csid;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public Time getTime() {
-		return time;
-	}
-	public void setTime(Time time) {
-		this.time = time;
+
 	}
 	
+	public void setDate(Date date) {
+
+		this.date=date;
+
+	}
+	
+
+	public int getTagid() {
+
+		return tagid;
+
+	}
+
+	public void setTagid(int tagid) {
+
+		this.tagid = tagid;
+
+	}
+
+	public int getCsid() {
+
+		return csid;
+
+	}
+
+	public void setCsid(int csid) {
+
+		this.csid = csid;
+
+	}
+
+	public String getText() {
+
+		return text;
+
+	}
+
+	public void setText(String text) {
+
+		this.text = text;
+
+	}
+
+	public Time getTime() {
+
+		return time;
+
+	}
+
+	public void setTime(Time time) {
+
+		this.time = time;
+
+	}
+
+	
+
 }
